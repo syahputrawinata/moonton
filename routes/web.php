42c1558e1cd6a92dce740654ef25318adcd5ae0b
syahputrawinata <sputrawinata23@gmail.com>
2025-04-29 10:08:00 +0700

    feat: Setup Spatie

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,6 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('admin', function() {
-    return 'Hi Admin';
-})->middleware('role:admin');
-
-Route::get('user', function() {
-    return 'Hi User';
-})->middleware('role:user');
-
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
